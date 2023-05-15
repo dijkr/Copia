87d551ca3f053632953d715fe4c73b53b6188dad
dijkr <vandijkr@pm.me>
2023-05-15 21:52:43 +0200

    Responsive

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,7 +6,7 @@
             <a href="#"><i class="fa-brands fa-square-twitter fa-xl" style="color: #ffffff;"></i></a>
         </div>
 
-        <div class="grid-item-footer-1"><h3>Nieuwsbrief aanmelding</h3>
+        <div class="grid-item-footer-1"><h3>Nieuwsbrief</h3>
             <a href="#"><i class="fa-solid fa-envelope fa-xl" style="color: #ffffff;"></i></a>
         </div>
 
@@ -14,14 +14,15 @@
         <div class="grid-item-footer-1"><h3>Klantenservice</h3>
             <a href="#"><i class="fa-brands fa-whatsapp fa-xl" style="color: #ffffff;"></i></a>
         </div>
-
+    </div>
 
         {{-- LINKS --}}
-        <div class="grid-item-footer-2">
+    <div class="grid-footer-2">
+{{--        <div class="grid-item-footer-2">--}}
         <a href="/" style="color: #1a202c">Algemene voorwaarden</a>
         <a href="/">Levering & verzending</a>
         <a href="/">Privacybeleid</a>
         <a href="/">Onze organisatie</a>
-        </div>
+{{--        </div>--}}
     </div>
 </footer>
