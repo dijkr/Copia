21539ea8c157ced19c7fbd385ff52f94a2c667f0
dijkr <vandijkr@pm.me>
2023-05-20 20:27:50 +0200

    layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,11 @@ var menuToggle = document.getElementById('menu-toggle');
 var menuItems = document.querySelector('.menu-items');
 var search = document.querySelector('.search');
 var user = document.querySelector('.fa-user');
+var header = document.querySelector('header');
 
 menuToggle.addEventListener('click', function() {
     menuItems.classList.toggle('show');
     search.classList.toggle('hide');
     user.classList.toggle('hide');
+    header.classList.toggle('background');
 });
