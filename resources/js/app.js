2531eed4d526c3c66f4cc6a8da003f2af963f51a
dijkr <vandijkr@pm.me>
2023-05-20 21:04:43 +0200

    menu-fixed

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import './bootstrap';
 
 /* HAMBURGER MENU BAR */
+var bar = document.querySelector('.fa-bars');
 var menuToggle = document.getElementById('menu-toggle');
 var menuItems = document.querySelector('.menu-items');
 var search = document.querySelector('.search');
@@ -8,6 +9,7 @@ var user = document.querySelector('.fa-user');
 var header = document.querySelector('header');
 
 menuToggle.addEventListener('click', function() {
+    bar.classList.toggle('style');
     menuItems.classList.toggle('show');
     search.classList.toggle('hide');
     user.classList.toggle('hide');
