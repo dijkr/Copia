752d663f0be66253fb0ff68e438c5db666405d2a
dijkr <vandijkr@pm.me>
2023-06-10 15:31:09 +0200

    fk-migration

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -8,16 +8,18 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    /* PRODUCTS - ALL PRODUCTS FROM ONE CATEGORY, SORTED PER SUBCATEGORY */
+    /* PRODUCTS - GROUPED BY SUBCATEGORY */
     public function showGroupedProducts (Request $request) {
+        dd($request);
         // Find the category
         $category = basename($request->getRequestUri());
         // Query products based on the category
         $products = Product::where('category', $category)->get();
         // Group products by subcategory
+        dd($products);
         $groupedProducts = $products->groupBy('Subcategory');
         // Query category data
-        $categoryData = Category::where('slug', $category)->first();
+        $categoryData = Product::where('slug', $category)->first();
         return view('producten', [
             'groupedProducts' => $groupedProducts,
             'category' => $categoryData
